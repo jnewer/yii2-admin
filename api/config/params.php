f96f2cbcc382371d8126166bd2ad8e0316ed3e73
jikang <jikang@jikangdeiMac.local>
2024-07-09 19:18:20 +0800

    update:config

--- api/config/params.php
+++ api/config/params.php
@@ -1,10 +1,6 @@
 <?php
 return [
     'origins'=>[
-        'http://localhost:3000',
-        'https://api.peconf.com',
-        'https://www.peconf.com',
-        'https://wechat.peconf.com',
-        'https://livepusher.peconf.com',
+        'http://api.yii2-admin.local',
     ],
 ];
